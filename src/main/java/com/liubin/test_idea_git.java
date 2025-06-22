e3c8e8e37302d6d618202c4436188aa4f0cc9647
SolitaryWayfarer <1366817857@qq.com>
2025-06-22 10:50:48 +0800

    测试IDEA提交代码

--- src/main/java/com/liubin/test_idea_git.java
+++ src/main/java/com/liubin/test_idea_git.java
@@ -4,5 +4,8 @@ public class test_idea_git {
     public static void main(String[] args) {
         // 1.测试idea推代码
         System.out.println("idea推代码");
+
+        // 2.测试修改代码并提交
+        System.out.println("idea测试修改代码并提交");
     }
 }
